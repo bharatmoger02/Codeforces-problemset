b3177c1eba2f163a4017ae7c8a4003b812b3bbd9
bharat moger <03bharatmgr@gmail.com>
2026-08-12 09:31:07 +0530

    A

---
+++ BitPlusPlus.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BitPlusPlus
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=0;
+        while(n>0)
+        {
+            String str=sc.next();
+            if(str.contains("++"))
+                x++;
+            else
+                x--;
+            n--;
+        }
+        System.out.println(x);
+    }
+}
