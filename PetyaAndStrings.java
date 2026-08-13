71c81066d3fe473d85cad827c3006afceacb627f
bharat moger <03bharatmgr@gmail.com>
2026-08-13 09:31:38 +0530

    112A

---
+++ PetyaAndStrings.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PetyaAndStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a=sc.nextLine().toLowerCase();
+        String b=sc.nextLine().toLowerCase();
+        for (int i = 0; i < a.length(); i++) {
+            if(a.charAt(i)>b.charAt(i)) {
+                System.out.println("1");
+                return;
+            }
+            if(a.charAt(i)<b.charAt(i)){
+                System.out.println("-1");
+                return;
+            }
+        }
+        System.out.println("0");
+    }
+}
