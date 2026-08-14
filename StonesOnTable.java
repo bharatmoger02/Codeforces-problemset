d2e749f87cfd611a991a5d9552deef88804ed3a0
bharat moger <03bharatmgr@gmail.com>
2026-08-14 20:11:33 +0530

    .....

--- StonesOnTable.java
+++ StonesOnTable.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class StonesOnTable {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        String s= sc.next();
+        int remove=0;
+        for (int i = 1; i < s.length(); i++) {
+            if(s.charAt(i)==s.charAt(i-1))
+                remove++;
+        }
+        System.out.println(remove);
+    }
 }
+
+
