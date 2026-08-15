0d9cd862b3dd493de5d64d3db6ae164ba8aacd9b
bharat moger <03bharatmgr@gmail.com>
2026-08-15 09:54:46 +0530

    .....

--- Football.java
+++ Football.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Football {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str= sc.next();
+        char[] chars = str.toCharArray();
+        int[] arr= new int[chars.length];
+
+        for (int i = 0; i < str.length(); i++) {
+            arr[i]=chars[i]-'0';
+        }
+        int count=1;
+        int j=0;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[j]!=arr[i]) {
+                j = i;
+                count = 1;
+            }
+            else
+                count++;
+            if(count==7) {
+                System.out.println("YES");
+                return;
+            }
+        }
+        System.out.println("NO");
+    }
 }
